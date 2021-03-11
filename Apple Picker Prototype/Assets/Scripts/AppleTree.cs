4a346f515266bc1c7c56cabccd06c8ca6fc56540
Olegsander78 <onkotov@gmail.com>
2021-03-11 11:26:24 +0500

    Наполнил сцену объектами

---
+++ Apple Picker Prototype/Assets/Scripts/AppleTree.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+    //Pattern creating Apple
+    public GameObject applePrefab;
+
+    //Velocity moving Apple
+    public float speed = 1f;
+
+    //The distance at which the direction of movement of the apple tree should change.
+    public float leftAndRightEdge = 10f;
+
+    //The likelihood of an accidental change in direction.
+    public float chanceToChangeDirection = 0.1f;
+
+    //The frequency at which the apples are instantiated.
+    public float secondsBetweenAppleDrops = 1f;
+         
+    void Start()
+    {
+        //Drop apples once a second.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Simple moving
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
+      //Change of direction
+    }
+}
